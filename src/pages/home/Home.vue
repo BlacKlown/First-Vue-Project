85e9372a90e13be05e31ac61c8b056f931687111
heben <867337666@qq.com>
2018-09-28 09:59:44 +0800

    首页轮播图制作

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
 <div>
   <home-header></home-header>
+  <home-swiper></home-swiper>
 </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Swiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    'home-header': Header
+    'home-header': Header,
+    'home-swiper': Swiper
   }
 }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        loop: true,
+        pagination: '.swiper-pagination'
+      },
+      swiperList: [{
+        id: '00',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/50/bf844a9b77a14502.jpg_750x200_12a36ed3.jpg'
+      }, {
+        id: '01',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/6a/847ac7b98f10e202.jpg_750x200_f0593cb5.jpg'
+      }, {
+        id: '02',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/1c/4847ea66072c7b02.jpg_750x200_c32457fb.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wapper >>> .swiper-pagination-bullet-active
+    background-color: #fff
+  .wapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.67%
+    background-color: #eee
+    img
+      width: 100%
+</style>
