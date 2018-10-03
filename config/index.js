dcb4d17716fd375d68e5ed2b09861275e54ca3a0
heben <867337666@qq.com>
2018-10-03 21:56:54 +0800

    上线设置

--- config/index.js
+++ config/index.js
@@ -12,9 +12,10 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
         '/api': {
-            target: 'http://localhost:8080',
+            target: 'http://localhost',
             pathRewrite: {
-                '^/api': 'static/mock'
+                // '^/api': 'static/mock'
+                '^/api': '/travel/api'
             }
         }
     },
@@ -57,7 +58,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/travel',
 
     /**
      * Source Maps
